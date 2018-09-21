004b478d0a3839abe038bd03102a5056c8e9680c
Serif Ozcan <serif.ozcan@gmail.com>
2018-09-21 15:27:22 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+const Pet = props => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
+  ]);
+};
+
+class App extends React.Component {
+  render() {
+    return React.createElement("div", {}, [
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "dog",
+        breed: "Havanese"
+      }),
+      React.createElement(Pet, {
+        name: "Pepper",
+        animal: "bird",
+        breed: "Cockatiel"
+      }),
+      React.createElement(Pet, {
+        name: "Doink",
+        animal: "cat",
+        breed: "Mixed"
+      })
+    ]);
+  }
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
